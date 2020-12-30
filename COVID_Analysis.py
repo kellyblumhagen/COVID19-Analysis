cdfc5a7f21bdba3bb44801932b5a7caa63616112
Kelly B <kellyblumhagen@gmail.com>
2020-12-30 04:45:02 -0500

    fixed output

--- COVID_Analysis.py
+++ COVID_Analysis.py
@@ -13,28 +13,24 @@ df = pd.read_csv("Resources/COVID19_data.csv")
 # Change column heading "Number of Mentions" to "Total_Deaths" so it's more understandable
 df.rename(columns={"Number of Mentions": "Total_Deaths"}, inplace=True)
 
-# Convert "Total" column to float for aggregating
+# Convert "Total_Deaths" column to float for aggregating
 df["Total_Deaths"] = df["Total_Deaths"].str.replace(",", "").astype(float)
 
-# Groupby "Total" to find total for each condition
+# Groupby "Total_Deaths" to find total for each condition
 df = df.groupby("Condition").Total_Deaths.sum().reset_index()
 
-# Rank totals in descending order
-df = df.sort_values("Total_Deaths", ascending=False)
-
-# Reset index and make it start with 1 (instad of default 0)
-df = df.reset_index(drop=True)
-df.index = df.index + 1
+# Rank total deaths in descending order
+df = df.sort_values("Total_Deaths", ascending=False, ignore_index=True)
 
 # Drop first two rows/conditions of dataframe ("COVID-19" and "All other conditions and causes") - not helpful for analyzing conditionss
 # Reset index
 df = df.drop([df.index[0], df.index[1]]).reset_index(drop=True)
 
-# Make index start with 1 (because displaying rank)
+# Make index start with 1 (so that output df will display ranking number next to each condition)
 df.index = df.index + 1
 
-# Save dataframe
-df.to_csv("Output/Top_10_DataFrame.csv", index=False)
+# Save dataframe as csv file
+df.to_csv("Output/Top_10_DataFrame.csv")
 
 ##########################################
 ########## MATPLOTLIB PIE CHART ##########
@@ -61,7 +57,7 @@ plt.pie(totals_top5, labels=names_top5, colors=colors,
         autopct="%1.1f%%", shadow=True, startangle=250, radius=1.2)
 
 # Chart title
-plt.title("Most Common Underlying Health Conditions for COVID-19 Deaths")
+plt.title("Top 5 Underlying Health Conditions for COVID-19 Deaths")
 
 # Save chart
 plt.savefig("Output/COVID_piechart.png")
